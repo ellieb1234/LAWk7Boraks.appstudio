7f916fb0c66b006e262fcbfb337a73a4518d0fb0
ellieb1234 <ellieboraks@creighton.edu>
2021-03-22 23:06:11 -0500

    customerDelete

--- LAWk7Boraks.appstudio/forms/customerDelete/customerDelete.js
+++ LAWk7Boraks.appstudio/forms/customerDelete/customerDelete.js
@@ -1,51 +1,42 @@
-/*
-query=""
-req={}
-results=[]
-netid="elb22116"
-pw="cozmox-5jibsI-sethyg"
+customerDelete.onshow = function() {
+     query = "SELECT * FROM customer;"
+     req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
 
-inptDelete = function() {
-  let companyNameDel = inptNameDel.value
-  let found = false
-  for (i = 0; i < results.length; i++) {
-    if (companyNameDel == results[i][1]) {
-      found = true
-      break
-    }
-  }
-  if (found == false)
-    lblDelete.textContent = "That customer name is not in the database."
-  else if (found == true) {
-    query = "DELETE FROM customer WHERE name = '" + companyNameDel + "'"
-    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=elb22116&query=" + query)
-    if (req.status == 200) //transit worked.{
-      if (req.responseText == 500)
-        lblDelete.textContent = `You have successfully deleted the pet named ${petNameDel}`
-      else
-        lblDelete.textContent = `There was a problem deleting ${petNameDel} from the database.`
-    }else
-      lblDelete.textContent = `Error: ${req.status}`
-  }
+     if (req.status == 200) { //transit trip worked. 
+          results = JSON.parse(req.responseText)
+          if (results.length == 0)
+               lblMessage2.value = "There are no customers in the database."
+          else {
+               let message = ""
+               for (i = 0; i < results.length; i++)
+                    message = message + results[i][1] + "\n"
+               txtaCustomers2.value = message
+          } // end else
+
+     } else // the transit didn't work - bad wifi? server turned off?
+          lblMessage2.value = "Error code: " + req.status
 }
 
-txtaCustomersDelete.onshow = function() {
-  query = "SELECT name FROM customer"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=elb22116&query=" + query)
-
-  if (req.status == 200) { //transit trip worked. 
-    results = JSON.parse(req.responseText)
-    console.log(`The results are: \n ${results}`)
-    if (results.length == 0)
-      lblDelete.value = "There are no customers in the database."
-    else {
-      let message = ""
-      for (i = 0; i < results.length; i++)
-        message = message + results[i][1] + "\n"
-      txtaCustomersDelete.value = message
-    } // end else
-
-  } else // the transit didn't work - bad wifi? server turned off?
-    txtaCustomers.value = "Error code: " + req.status
+btnSubmit.onclick = function() {
+     let customerDel = inptNameDel.value
+     let found = false
+     for (i = 0; i < results.length; i++) {
+          if (customerDel == results[i][1]) {
+               found = true
+               break
+          }
+     }
+     if (found == false)
+          lblMessage2.value = "That customer name is not in the database."
+     else if (found == true) {
+          query = "DELETE FROM customer WHERE name = '" + customerDel + "'"
+          req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+          if (req.status == 200) { //transit worked.
+               if (req.responseText == 500)
+                    lblMessage2.value = `You have successfully deleted the customer named ${customerDel}`
+               else
+                    lblMessage2.value = `There was a problem deleting ${customerDel} from the database.`
+          } else
+               lblMessage2.value = `Error: ${req.status}`
+     }
 }
-*/
